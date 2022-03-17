c19700b66be614fb13d7bdd59fae9e5a471ad49c
Ceren <cerenaltay.lsb@gmail.com>
2022-03-17 11:04:00 +0300

    Add project files.

---
+++ StringCalculatorTask/StringCalculatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StringCalculatorTask
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
