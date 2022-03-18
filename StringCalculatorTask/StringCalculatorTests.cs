4c4eb019780a5e42794dea9836072347307142d8
Ceren <cerenaltay.lsb@gmail.com>
2022-03-18 20:22:28 +0300

    BonusTask1 test9 red-delimiters with any length not supported

--- StringCalculatorTask/StringCalculatorTests.cs
+++ StringCalculatorTask/StringCalculatorTests.cs
@@ -84,5 +84,14 @@ namespace StringCalculatorTask
 
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("//[***]\n1***2***3", 6)]
+        public void Add_ContainsDelimitersWithAnyLength_ReturnsSum(string numbers, int expected)
+        {
+            var result = _stringCalculator.Add(numbers);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
